327e93d3798d39e7e0ce586cfc1be4c43a0f509b
Han JiHun <hjh_3229@naver.com>
2023-06-15 16:08:49 +0900

    RequestDto

--- src/main/java/com/sparta/myblogbackend/dto/BlogRequest.java
+++ src/main/java/com/sparta/myblogbackend/dto/BlogRequest.java
@@ -1,4 +1,11 @@
 package com.sparta.myblogbackend.dto;
 
+import lombok.Getter;
+
+@Getter
 public class BlogRequest {
+    private String title;
+    private String username;
+    private String contents;
+    private String password;
 }
